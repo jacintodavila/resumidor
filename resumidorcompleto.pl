57e73722bb58c4dfd99e2c2061b19d7567e2a962
jacintodavila <jacinto.davila@gmail.com>
2020-03-28 11:41:53 -0400

    Actualizando gramatica para resolver error de prediccion

--- resumidorcompleto.pl
+++ resumidorcompleto.pl
@@ -1,7 +1,7 @@
 % resumidorcompleto.pl
-% text summarizer for spanish and english texts. 
+% text summarizer for spanish and english texts.
 %
-%    Copyright (C) 2015 
+%    Copyright (C) 2015
 %    Jacinto Dávila, Hilda Yelitza Contreras, M. Marilú Parra, Jose Lopez
 %
 %    This program is free software: you can redistribute it and/or modify
@@ -17,6 +17,8 @@
 %    You should have received a copy of the GNU Affero General Public License
 %    along with this program.  If not, see <http://www.gnu.org/licenses/>
 %
+
+:- discontiguous claridad/2, es_conjuncion/1.
 
 :- [resumidorpaso1entradas].
 :- [resumidorpaso2gramatica].
--- resumidorpaso2gramatica.pl
+++ resumidorpaso2gramatica.pl
@@ -1,9 +1,10 @@
-% resumidorpaso2entradas.pl version 1.0 
+% resumidorpaso2entradas.pl version 1.1
 % parte de resumidorcompleto.pl
+% version 1.1: cambios para mejorar las gramaticas
 %
-% ensamblado por Jacinto Dávila 
-% 
-%    Copyright (C) 2015 
+% ensamblado por Jacinto Dávila
+%
+%    Copyright (C) 2015
 %    Jacinto Dávila, Hilda Yelitza Contreras, M. Marilú Parra, Jose Lopez
 %
 %    This program is free software: you can redistribute it and/or modify
@@ -26,11 +27,11 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % claridad(+Parrafo,-ParrafoWilliams)
-% Implementa el concepto de Claridad de Williams. La claridad local de 
-% oración es la estructura fija del discurso escrito: sujeto, verbo y 
+% Implementa el concepto de Claridad de Williams. La claridad local de
+% oración es la estructura fija del discurso escrito: sujeto, verbo y
 % complemento.
-% Procesa el párrafo de entrada (dividido en tokens) y retorna un párrafo 
-% donde cada oración es una lista de la siguiente forma: 
+% Procesa el párrafo de entrada (dividido en tokens) y retorna un párrafo
+% donde cada oración es una lista de la siguiente forma:
 % [sujeto(Sujeto),verbo(Verbo),complemento(Complemento)]
 
 claridad([],[]).
@@ -44,11 +45,14 @@ claridad([],[]).
 % se tiene el primer verbo de la oración.
 
 claridad([Oracion|RestoParrafo],[OracionWilliams|RestoParrafoWilliams]) :-
+      write(Oracion),
       setof(OracionW,s(OracionW,Oracion,[]),Lista),
-      oracion(Lista,OracionWilliams),OracionWilliams = [sujeto(L1),verbo(Vr),complemento(L2)],
-      % prohibido write desde acá. Toda salida desde paso5. 
+      oracion(Lista,OracionWilliams), % OracionWilliams = [sujeto(L1),verbo(Vr),complemento(L2)],
+      % prohibido write desde acá. Toda salida desde paso5.
       % write(L1), nl, nl, write(Vr), nl, nl, write(L2),
-      assert(relacion(OracionWilliams)),
+      % guardar todas las relaciones
+      assert_list(Lista),
+      % assert(relacion(OracionWilliams)),
       % verb_aux(Vr)-> mandar(L2);
       % mostrar(L2),
       % mostrar(L1),
@@ -56,6 +60,11 @@ claridad([Oracion|RestoParrafo],[OracionWilliams|RestoParrafoWilliams]) :-
 
 claridad([_|RestoParrafo],RestoParrafoWilliams) :-
     claridad(RestoParrafo,RestoParrafoWilliams).
+
+assert_list([]).
+assert_list([Clausula|Resto]) :-
+      assert(relacion(Clausula)),
+      assert_list(Resto).
 
 % oracion, retorne la oracion con menor sujeto que tenga el verbo mas complejo.
 
@@ -68,24 +77,29 @@ oracion([Oracion1,Oracion2|_],Oracion2) :-
 oracion([Oracion1,_|_],Oracion1).
 
 % s(?Estructura,+Oracion,+ListaVacia)
-% Implementa una gramática basada en verbos, sólo reconoce el verbo principal 
+% Implementa una gramática basada en verbos, sólo reconoce el verbo principal
 % de la oración (a través de un diccionario de verbos). Lo que esta antes del
 % verbo es el sujeto y lo que esta después es el complemento. Usa DCG.
-% Contienen la definición básica de las oraciones: frase nominal, frase verbal 
+% Contienen la definición básica de las oraciones: frase nominal, frase verbal
 % (verbo y complemento). Es decir s --> np,vp
-% Retorna la estructura (Estructura) de la oración (Oracion), en términos 
+% Retorna la estructura (Estructura) de la oración (Oracion), en términos
 % de sujeto, verbo y complemento (Retorna una lista con estos elementos).
 
 s([NP,V,C]) --> np(NP), vp(V,C).
+s([NP,V,C]) --> np(NP), vp(V,C), ac(_).
+s([NP,V,C]) --> np(_NP0), vp(_V0,_C0), ac([NP,V,C]). % una clausula incluida
+
+ac([NP,V,C]) --> semicolon, np(NP), vp(V,C).
 
 % frase nominal (np)
 % Frase que contiene algún nombre (sustantivo), correspondiente al sujeto
-% sintáctico de la oración. Considera algunas frases o colocaciones que 
+% sintáctico de la oración. Considera algunas frases o colocaciones que
 % tienen conflictos con los verbos.
 
-np(sujeto([C1,C2|Resto])) --> colocacion(C1,C2), !, np_resto(Resto).
-np(sujeto([C1,C2,C3|Resto])) --> colocacion(C1,C2,C3), !, np_resto(Resto).
-np(sujeto([X|Resto])) --> [X|Resto].
+% np(sujeto([C1,C2|Resto])) --> colocacion(C1,C2), !, np_resto(Resto).
+% np(sujeto([C1,C2,C3|Resto])) --> colocacion(C1,C2,C3), !, np_resto(Resto).
+
+np(sujeto([X|Resto])) --> [X|Resto], {not(member(';', Resto))}.
 
 np_resto([X|Resto]) --> [X|Resto].
 
@@ -102,7 +116,7 @@ vp(V,C) --> verb_aux(V), comp(C).
 vp(V,C) --> verb(V), comp(C).
 vp(V,C) --> verb_ont(V), comp(C).
 
-%% oración impersonal: usa el pronombre "se", el sujeto permisible es la 
+%% oración impersonal: usa el pronombre "se", el sujeto permisible es la
 %% generalidad de la gente.
 
 verb_impersonal([V1,V2,V3,V4]) -->  impersonal(V1,V2), verb_compuesto([V3,V4]).
@@ -113,22 +127,28 @@ impersonal(sujeto_verbo([se]), sujeto_verbo([le])) --> [se, le].
 impersonal(sujeto_verbo([se]), sujeto_verbo([les])) --> [se, les].
 impersonal(sujeto_verbo([se])) --> [se].
 %% si el verbo no esta en el diccionario de verbos
-resto_impersonal(verbo([X])) --> [X]. 
+resto_impersonal(verbo([X])) --> [X].
 
 %% casos de verbos compuestos
 
 % Los verbos en participio solo pueden estar despues de los verbos
 % auxiliares. Si no es asi son sustantivos o adjetivos.
 
-verb_compuesto([V1,V2]) --> verb_aux(V1), verb_part(V2). 
+verb_compuesto([V1,V2]) --> verb_aux(V1), verb_part(V2).
 verb_compuesto([V1,V2]) --> verb_aux(V1), verb(V2).
 verb_compuesto([V1,V2]) --> verb_aux(V1), verb_aux(V2).
 verb_compuesto([V1,V2]) --> verb(V1), verb(V2).
 
-% complemento de la frase verbal
+% complemento de la frase verbal (al menos una palabra o símnbolo)
 
-comp(complemento([Y|Resto])) --> [Y|Resto].
+% a complement must not have a ; inside
+comp(complemento([Y|Resto])) --> [Y|Resto],
+                                 {not(member(';', [Y|Resto]))}.
 
-
+% complement must have al least one word or a semicolon
+complement([], [], []).
+complement([';'], [';'|R], R) :- !.  % stop at ; forget after it
+complement([H|Rn], [H|R], F) :-      % jumps over one word
+    complement(Rn, R, F).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% fin resumidorpaso2gramatica.pl
